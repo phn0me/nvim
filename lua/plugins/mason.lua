71cae9c7dae848523b420f7a1bdb2ce9d2c4a32a
phn0me <phn0me@pm.me>
2025-05-13 21:06:06 +0200

    updating nvim

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -13,46 +13,35 @@ return {
     dependencies = "mason.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rust_analyzer" },
+        ensure_installed = { "lua_ls" },
       })
     end,
   },
 
   {
     "neovim/nvim-lspconfig",
-    dependencies = "mason-lspconfig.nvim",
+    dependencies = { "mason.nvim", "mason-lspconfig.nvim" },
     config = function()
-      local lspconfig = require("lspconfig")
+      -- 2) Expand and point to the right directory
+      local love_api_lua = vim.fn.stdpath("config") .. "/lua/love-api"
 
-      lspconfig.lua_ls.setup({
+      require("lspconfig").lua_ls.setup({
         settings = {
           Lua = {
             runtime = {
               version = "LuaJIT",
-              -- Make sure the server can resolve modules on your package.path
               path = vim.split(package.path, ";"),
             },
             diagnostics = {
-              globals = { "love" },
+              globals = { "love" }, -- tell it that `love` is a built-in global
             },
             workspace = {
-              -- https://github.com/love2d-community/love-api.git
-              -- Combine Neovim’s runtime with your Love2D stubs folder
-              library = vim.list_extend(vim.api.nvim_get_runtime_file("", true), { "~/.config/nvim/lua/love-api" }),
+              -- include Neovim runtime AND your Love2D stubs folder
+              library = vim.list_extend(vim.api.nvim_get_runtime_file("", true), { love_api_lua }),
             },
-            completion = {
-              callSnippet = "Replace",
-            },
-            telemetry = {
-              enable = false,
-            },
+            telemetry = { enable = false },
           },
         },
-      })
-
-      -- Rust
-      lspconfig.rust_analyzer.setup({
-        -- your rust-specific settings here
       })
     end,
   },
